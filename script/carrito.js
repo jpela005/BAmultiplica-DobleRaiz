42a6e31ef00dec928ba791fe06218df7d1b6b533
Julian Pela <129326686+jpela005@users.noreply.github.com>
2023-06-02 22:33:34 -0300

    proyecto bodega doble raiz
    pagina web de bodega con index, y varias otras pestañas, en la cual encontramos imagenes, videos iframes , css, javascript, entre otras cosas.

---
+++ script/carrito.js
@@ -0,0 +1,42 @@
+  const cantidadInput = document.querySelector(".cant-producto");
+  const precio = document.getElementById("precio");
+  const subtotalInput = document.getElementById("subtotal");
+  const totalInput = document.getElementById("precio-total");
+  const descuentoInput = document.getElementById("input-descss");
+
+  function calcularTotal() {
+    const cantidad = parseInt(cantidadInput.value);
+    const precioUnitario = parseFloat(precio.textContent);
+    const subtotal = precioUnitario * cantidad;
+
+    subtotalInput.value = subtotal.toFixed(2);
+
+    let totalPagar = subtotal;
+    const descuento = descuentoInput.value;
+
+    if (descuento === "15EnMiDobleRaiz") {
+      const codDescuento = 15;
+      totalPagar = aplicarDescuento(totalPagar, codDescuento);
+    } else if (descuento === "PlavacMali") {
+      const codDescuento = 20;
+      totalPagar = aplicarDescuento(totalPagar, codDescuento);
+    } else if (descuento === "35BodegaDobleRaiz") {
+      const codDescuento = 35;
+      totalPagar = aplicarDescuento(totalPagar, codDescuento);
+    } else if (descuento === "") {
+      const codDescuento = 0;
+      totalPagar = aplicarDescuento(totalPagar, codDescuento);
+    
+    }
+
+    totalInput.value = totalPagar.toFixed(2);
+  }
+
+  function aplicarDescuento(total, codDescuento) {
+    return total - (total * codDescuento) / 100;
+  }
+
+
+  cantidadInput.addEventListener("change", calcularTotal);
+  descuentoInput.addEventListener("input", calcularTotal);
+
---
+++ script/script.js
@@ -0,0 +1,80 @@
+//  menu desplegable
+ 
+ function toggleDropdown() {
+   var dropdownMenu = document.querySelector('.dropdown-menu');
+   dropdownMenu.classList.toggle('show');
+ }
+ 
+ 
+
+//contador de caractares de textarea contacto
+
+ const contadorTextArea = document.getElementById("textarea-contacto");
+ const caractUtilizados = document.getElementById("caracteres-utilizados");
+ const limite = 500;
+
+ contadorTextArea.onkeyup = function() {
+   let caractEscritos = contadorTextArea.value.length;
+   caractUtilizados.innerHTML = (caractEscritos) + "/500";
+ };
+
+ //comprobante de contacto
+
+ function enviado() {
+  let nombreInput = document.getElementById('nombre');
+  let apellidoInput = document.getElementById('apellido');
+  let emailInput = document.getElementById('email');
+  let emailReal = /^[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}$/;
+  
+
+  if (nombreInput.value === '') {
+    alert('Por favor, ingrese un nombre.');
+    return false;
+  }
+
+  if (apellidoInput.value === '') {
+    alert('Por favor, ingrese un apellido.');
+    return false;
+  }
+
+  if (emailInput.value === '') {
+    alert('Por favor, ingrese un correo electrónico.');
+    return false;
+  }
+
+  if (!emailReal.test(emailInput.value)) {
+    alert("Por favor, ingrese un correo electrónico válido.");
+    return false;
+  }
+
+  else
+  alert('enviado')
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
